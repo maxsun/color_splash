e6d9ead23ac7bb7e328d2602af3039468c954b68
Max Sun <max@maxs-mbp.home>
2019-08-10 23:25:50 -0400

    Basic demo

---
+++ index.js
@@ -0,0 +1,35 @@
+
+/**
+ * Opens a ".window".
+ * 1. Lowers all other windows' zIndex
+ * 2. Collapse every window
+ * 3. Un-collapse window (raise zIndex and remove .collapsed)
+ * @param {Node} window The first number.
+ */
+function openWindow(window) {
+  const windows = document.querySelectorAll('.window');
+  windows.forEach((w) => {
+    w.style.zIndex -= 1;
+  });
+
+  window.classList.add('notransition');
+  window.classList.add('collapsed');
+
+  setTimeout(() => {
+    window.classList.remove('notransition');
+    window.style.zIndex = windows.length + 1;
+    window.classList.remove('collapsed');
+  }, 10);
+}
+
+window.onload = function() {
+  const windows = document.querySelectorAll('.window');
+  let i = 0;
+  openWindow(windows[i]);
+  document.body.onclick = function(e) {
+    document.body.style.setProperty('--mouse-x', e.clientX + 'px');
+    document.body.style.setProperty('--mouse-y', e.clientY + 'px');
+    i = (i + 1) % windows.length;
+    openWindow(windows[i]);
+  };
+};
